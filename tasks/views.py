0f07ff5ec7042c7dc2b96e745136296549e28d33
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-19 22:10:07 +0530

    some starter code

--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,35 @@
-# Add your Views Here
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from .models import Task
+
+
+def task_view(request):
+    search_task = request.GET.get("search")
+    tasks = Task.objects.all().filter(deleted=False, completed=False)
+    if search_task:
+        tasks = tasks.filter(title__icontains=search_task)
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def add_task_view(request):
+    task_value = request.GET.get("task")
+    task = Task(title=task_value)
+    if request.user.is_authenticated:
+        task.user = request.user
+    task.save()
+    return HttpResponseRedirect("/tasks/")
+
+
+def delete_task_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return HttpResponseRedirect("/tasks/")
+
+
+def complete_task_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.completed = True
+    task.save()
+    return HttpResponseRedirect("/tasks/")
